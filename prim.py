16f71b29ff4b1316763083dbad0d894c7e411444
NeusAiri <josehernandez35308@gmail.com>
2022-11-10 11:06:42 -0600

    primer streamlit lab

--- prim.py
+++ prim.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 nrow = 100
 
-nameL = 'dataset.csv'
+nameL = 'https://raw.githubusercontent.com/NeusAiriGit/streamlit_lab/2ce36a1deaaa3962cb3b79f89b8c4553d69ab1dc/dataset.csv?token=GHSAT0AAAAAAB2XY6OHRP6XGESNPLKCMCKMY3NHVDA'
 st.title('Read csv')
 
 
